48396a39acae9c88702ad541bb1941a597da3ae2
SaiManidharKanthati <119723865+SaiManidharKanthati@users.noreply.github.com>
2022-12-28 20:50:24 +0530

    #include <stdio.h> int main() {    int y;    scanf("%d",&y);    if (((y % 4 == 0) && (y % 100!= 0)) || (y%400 == 0))    {       printf("True");    }    else    {       printf("False");    } }

--- Program_to_find_out_the_entered_year_is_leap_year_or_not.c
+++ Program_to_find_out_the_entered_year_is_leap_year_or_not.c
@@ -3,10 +3,10 @@ int main()
 {
    int y; 
    scanf("%d",&y);
-   if (((y % 4 == 0) && (y % 100!= 0)) || (y%400 == 0))
+   if (((y % 4 == 0) && (y % 100!= 0)) || (y%400 == 0))  
    {
       printf("True");
-   }  
+   }
    else
    {
       printf("False");
