48765bebb716b5456562a2d5218235e2c3a3d32a
SaiManidharKanthati <119723865+SaiManidharKanthati@users.noreply.github.com>
2023-01-06 11:57:17 +0530

    #include<stdio.h> int main() { 	char str[100]; 	int i,j,vc=1; 	scanf("%[^ ]s",str); 	for(i=0;str[i]!=NULL;i++) 	{ 		if(str[i]==' ') 		{ 			vc++; 		} 	} 	printf("%d",vc); }

---
+++ Count_no._of_words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,j,vc=1;
+	scanf("%[^
+]s",str);
+	for(i=0;str[i]!=NULL;i++)
+	{
+		if(str[i]==' ')
+		{
+			vc++;
+		}
+	}
+	printf("%d",vc);
+}
